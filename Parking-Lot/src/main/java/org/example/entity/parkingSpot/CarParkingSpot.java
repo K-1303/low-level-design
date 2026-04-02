814bea4a0dc06b1d4777a07880f41ff6e4cce9ca
K-1303 <kavyadua03@gmail.com>
2026-04-03 00:19:16 +0530

    Completed with parking lot lld

---
+++ Parking-Lot/src/main/java/org/example/entity/parkingSpot/CarParkingSpot.java
@@ -0,0 +1,13 @@
+package org.example.entity.parkingSpot;
+
+import org.example.factory.Vehicle;
+
+public class CarParkingSpot extends ParkingSpot {
+    public CarParkingSpot(int spotNumber, String spotType) {
+        super(spotNumber, spotType);
+    }
+    @Override
+    public boolean canParkVehicle(Vehicle vehicle) {
+        return "Car".equalsIgnoreCase(vehicle.getVehicleType());
+    }
+}
